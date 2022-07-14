c7326fbcc0982c7a8d973abe7c25c2a05ad26d0b
Oyinloye David Kayode <oyinsbiz@gmail.com>
2022-07-14 13:18:37 -0700

    Task 0

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,4 @@
+char *rot13(char *s);
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
@@ -6,4 +7,4 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
-7-leet.c
+char *leet(char *s);
