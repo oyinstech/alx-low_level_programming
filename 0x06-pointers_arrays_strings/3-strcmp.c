eb7c080a2b12ddc2b1c92d43cf6bead847410d1b
Oyinloye David Kayode <oyinsbiz@gmail.com>
2022-07-14 13:59:51 -0700

    Task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: The difference between s1 and s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2)
+	{
+		if (*s1 != *s2)
+		{
+			return (*s1 - *s2);
+		}
+		s1++;
+		S2++;
+	}
+	return (*s1 - *s2);
+}
