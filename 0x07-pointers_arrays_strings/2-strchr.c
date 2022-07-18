9cf5978ff76809acbd234c82b1a89147d700f6d4
Oyinloye David Kayode <oyinsbiz@gmail.com>
2022-07-18 08:26:57 -0700

    Task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strchr - locates a character in a string
+ * @s: string to search
+ * @c: char to find
+ * Return: a string
+ */
+char *_strchr(char *s, char c)
+{
+	int i, b;
+
+	i = 0;
+
+	while (s[i])
+		i++;
+	for (b = 0; b < a; b++)
+		if (c == s[b])
+			s += b;
+	return (s);
+	{
+		return ('\0');
+	}
+}
