2af165666bfdf7a75b06bcc911b50b2cbc0252c2
Oyinloye David Kayode <oyinsbiz@gmail.com>
2022-08-11 21:01:58 -0700

    Task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * first - function executed before main
+ * Return: void
+ */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
