31dceb9858850e4991ddf24df30d011ff17329b5
Oyinloye David Kayode <oyinsbiz@gmail.com>
2022-08-16 15:19:38 -0700

    Task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - Frees a listint_t list
+ * @head: pointer to the listint_t list to be freed
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (head && *head)
+	{
+		hold = *head;
+		*head = (**head).next;
+		free(hold);
+	}
+}
